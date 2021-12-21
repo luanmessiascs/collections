e3ecb2f0300b69b17d6603a8e401f12071b1d46f
Luan Messias <luan44058@gmail.com>
2021-12-20 21:50:55 -0300

    Tentando fazer a ligação das páginas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'collections#index'
-  resources :collections
-  resources :albums
-  resources :items
+  resources :collections do
+    resources :albums do
+      resources :items
+    end
+  end
 end
